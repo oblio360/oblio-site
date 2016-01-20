33fe076dc9598df4cced550d19594552df62ce58
Constantin Caraivan <costincaraivan@gmail.com>
2016-01-20 22:00:45 +0100

    Final polishing work before publishing.

--- js/latest-articles.js
+++ js/latest-articles.js
@@ -1,5 +1,5 @@
 $.ajax({
-    url: "http://hearthstone.oblio360.com/api/articles.json",
+    url: "http://dota.oblio360.com/api/articles.json",
     success: function( data ) {
         index = 0;
         for(article in data.articles) {
@@ -7,8 +7,7 @@ $.ajax({
             name = article;
             url = data.articles[article].url;
             article_link = "<div class='latest-article'><a href=\"" + url + "\">" + name + "</a></div>";
-            console.log(article_link);
-            $(".latest-articles").append(article_link);
+            $(".latest-dota-articles").append(article_link);
             if(index > 2) {
                 return false;
             }
@@ -20,6 +19,65 @@ $.ajax({
 });
 
 $.ajax({
+    url: "http://hearthstone.oblio360.com/api/articles.json",
+    success: function( data ) {
+        index = 0;
+        for(article in data.articles) {
+            index = index + 1;
+            name = article;
+            url = data.articles[article].url;
+            article_link = "<div class='latest-article'><a href=\"" + url + "\">" + name + "</a></div>";
+            $(".latest-hearthstone-articles").append(article_link);
+            if(index > 2) {
+                return false;
+            }
+        }
+    },
+    error: function(errorjqXHR, error, errorThrown) {
+        console.log(error);
+    }
+});
+
+$.ajax({
+    url: "http://blog.oblio360.com/api/articles.json",
+    success: function( data ) {
+        index = 0;
+        for(article in data.articles) {
+            index = index + 1;
+            name = article;
+            url = data.articles[article].url;
+            article_link = "<div class='latest-article'><a href=\"" + url + "\">" + name + "</a></div>";
+            $(".latest-blog-articles").append(article_link);
+            if(index > 2) {
+                return false;
+            }
+        }
+    },
+    error: function(errorjqXHR, error, errorThrown) {
+        console.log(error);
+    }
+});
+
+
+$.ajax({
+    url: "http://dota.oblio360.com/api/tags.json",
+    success: function( data ) {
+        for(tag in data.tags) {
+            name = tag;
+            url = data.tags[tag];
+            if(name !== "") {
+                tag_link = "<div class='article-tag'><a href=\"" + url + "\">" + name + "</a></div>";
+                $("#dota-tags").append(tag_link);
+            }
+        }
+    },
+    error: function(errorjqXHR, error, errorThrown) {
+        console.log(error);
+    }
+});
+
+
+$.ajax({
     url: "http://hearthstone.oblio360.com/api/tags.json",
     success: function( data ) {
         for(tag in data.tags) {
@@ -27,8 +85,24 @@ $.ajax({
             url = data.tags[tag];
             if(name !== "") {
                 tag_link = "<div class='article-tag'><a href=\"" + url + "\">" + name + "</a></div>";
-                console.log(tag_link);
-                $(".tag-card").append(tag_link);
+                $("#hearthstone-tags").append(tag_link);
+            }
+        }
+    },
+    error: function(errorjqXHR, error, errorThrown) {
+        console.log(error);
+    }
+});
+
+$.ajax({
+    url: "http://blog.oblio360.com/api/tags.json",
+    success: function( data ) {
+        for(tag in data.tags) {
+            name = tag;
+            url = data.tags[tag];
+            if(name !== "") {
+                tag_link = "<div class='article-tag'><a href=\"" + url + "\">" + name + "</a></div>";
+                $("#blog-tags").append(tag_link);
             }
         }
     },
